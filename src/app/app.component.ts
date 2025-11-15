6f6bc8fa93a0c896171cc4aef3e0e6ea8a65ac0d
HarshJani1 <harshjani011@gmail.com>
2025-11-15 20:16:13 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-19-tut';
+ count=0;
+
+
+ handleClick(value:string){
+  if(value == 'minus'){
+    if(this.count!=0) this.count--;
+  }else if(value == 'reset'){
+    this.count = 0;
+  }
+  else{
+    this.count++;
+  }
+ }
+
 }
